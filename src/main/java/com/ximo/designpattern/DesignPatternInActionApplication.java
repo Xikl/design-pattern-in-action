d46c9435f6130c63e78efad79553aab42d4ff7b9
朱文赵 <zwz@servyou.com.cn>
2018-03-06 13:55:42 +0800

    init

---
+++ src/main/java/com/ximo/designpattern/DesignPatternInActionApplication.java
@@ -0,0 +1,12 @@
+package com.ximo.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternInActionApplication.class, args);
+	}
+}
