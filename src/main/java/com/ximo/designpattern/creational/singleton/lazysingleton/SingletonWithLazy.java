e87690d5de65035d54e664129cad922d932cdb53
xikl <1392936858@qq.com>
2019-03-26 21:11:35 +0800

    同步修改！

--- src/main/java/com/ximo/designpattern/creational/singleton/lazysingleton/SingletonWithLazy.java
+++ src/main/java/com/ximo/designpattern/creational/singleton/lazysingleton/SingletonWithLazy.java
@@ -10,17 +10,24 @@ public class SingletonWithLazy {
 
     private static SingletonWithLazy instance;
 
+    /** 同步对象 */
+    private static final Object object = new Object();
+
     private SingletonWithLazy() {
     }
 
     /**
      * 非线程安全
+     * 通过同步锁来保证线程安全 这里只同步一个对象 而不用拿到整个class的锁
+     * 如果将synchronized放到静态方法中 和 synchronized(SingletonWithLazy.class) 是一样的效果 都是要拿到整个class的锁
      *
      * @return
      */
     public static SingletonWithLazy getInstance() {
-        if (instance == null) {
-            instance = new SingletonWithLazy();
+        synchronized (object) {
+            if (instance == null) {
+                instance = new SingletonWithLazy();
+            }
         }
         return instance;
     }
