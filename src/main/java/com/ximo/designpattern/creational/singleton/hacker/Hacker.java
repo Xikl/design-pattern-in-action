d0e0dcb543e2e4b8d7dd3ed6cc63ac20ee437d71
朱文赵 <1392936858@qq.com>
2019-03-31 19:37:19 +0800

    采用反射进行创建单例对象

--- src/main/java/com/ximo/designpattern/creational/singleton/hacker/Hacker.java
+++ src/main/java/com/ximo/designpattern/creational/singleton/hacker/Hacker.java
@@ -3,10 +3,11 @@ package com.ximo.designpattern.creational.singleton.hacker;
 import com.ximo.designpattern.creational.singleton.innerholder.SingletonWithHolder;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * 单例模式的破坏者
- *
  *
  * @author xikl
  * @date 2019/3/28
@@ -14,13 +15,14 @@ import java.io.*;
 public class Hacker {
 
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        hackByObjectOutputStream();
+    public static void main(String[] args) throws Exception {
+//        hackByObjectOutputStream();
+        hackByReflect();
     }
 
 
     private static void hackByObjectOutputStream() throws IOException, ClassNotFoundException {
-        String fileName = "oos_instance";
+        String fileName = "oos_instance.txt";
         SingletonWithHolder instance = SingletonWithHolder.getInstance();
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(fileName)));
         oos.writeObject(instance);
@@ -31,6 +33,17 @@ public class Hacker {
         System.out.println(singletonWithHolder);
         System.out.println(instance);
         System.out.println(instance == singletonWithHolder);
+    }
+
+    private static void hackByReflect() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Constructor con = SingletonWithHolder.class.getDeclaredConstructor();
+        con.setAccessible(true);
+        SingletonWithHolder newInstance = (SingletonWithHolder) con.newInstance();
+
+        SingletonWithHolder instance = SingletonWithHolder.getInstance();
+
+        System.out.println(instance);
+        System.out.println(newInstance);
     }
 
 
